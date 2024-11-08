18d9996749511c0f7313e352c7d005e1f52f0edb
mfaizasysyauqi <109415400+mfaizasysyauqi@users.noreply.github.com>
2024-11-08 19:25:21 +0700

    Update GetNoteUseCase.java

--- src/main/java/com/simplenotes/domain/usecases/note/GetNoteUseCase.java
+++ src/main/java/com/simplenotes/domain/usecases/note/GetNoteUseCase.java
@@ -1,5 +1,17 @@
-package main.java.com.simplenotes.domain.usecases.note;
+// GetNoteUseCase.java
+package com.simplenotes.domain.usecases.note;
+
+import com.simplenotes.domain.entities.Note;
+import com.simplenotes.domain.repositories.NoteRepository;
 
 public class GetNoteUseCase {
-    
+    public final NoteRepository noteRepository;
+
+    public GetNoteUseCase(NoteRepository noteRepository) {
+        this.noteRepository = noteRepository;
+    }
+
+    public Note execute(Long id) {
+        return noteRepository.findById(id);
+    }
 }

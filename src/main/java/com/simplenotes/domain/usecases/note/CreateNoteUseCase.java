66c4a02f01dbb6cd2e00b877b93fcd883b485f54
mfaizasysyauqi <109415400+mfaizasysyauqi@users.noreply.github.com>
2024-11-08 19:25:54 +0700

    Update CreateNoteUseCase.java

--- src/main/java/com/simplenotes/domain/usecases/note/CreateNoteUseCase.java
+++ src/main/java/com/simplenotes/domain/usecases/note/CreateNoteUseCase.java
@@ -1,5 +1,18 @@
-package main.java.com.simplenotes.domain.usecases.note;
+// CreateNoteUseCase.java
+package com.simplenotes.domain.usecases.note;
+
+import com.simplenotes.domain.entities.Note;
+import com.simplenotes.domain.repositories.NoteRepository;
 
 public class CreateNoteUseCase {
-    
+    private final NoteRepository noteRepository;
+
+    public CreateNoteUseCase(NoteRepository noteRepository) {
+        this.noteRepository = noteRepository;
+    }
+
+    public Note execute(String title, String content, Long categoryId) {
+        Note note = new Note(null, title, content, categoryId);
+        return noteRepository.save(note);
+    }
 }

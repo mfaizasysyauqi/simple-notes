305eeac1fc15dd748dc68bbc708f1ad96747c3e1
mfaizasysyauqi <109415400+mfaizasysyauqi@users.noreply.github.com>
2024-11-08 19:22:42 +0700

    Update SqliteConnection.java

--- src/main/java/com/simplenotes/infrastructure/persistence/sqlite/SqliteConnection.java
+++ src/main/java/com/simplenotes/infrastructure/persistence/sqlite/SqliteConnection.java
@@ -1,5 +1,14 @@
-package main.java.com.simplenotes.infrastructure.persistence.sqlite;
+// src/main/java/com/simplenotes/infrastructure/persistence/sqlite/SqliteConnection.java
+package com.simplenotes.infrastructure.persistence.sqlite;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class SqliteConnection {
+    private static final String URL = "jdbc:sqlite:simplenotes.db";
     
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL);
+    }
 }

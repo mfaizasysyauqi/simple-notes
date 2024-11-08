ffee7e7d5a9a2fc43556eeaf52905c6fdde771ed
mfaizasysyauqi <109415400+mfaizasysyauqi@users.noreply.github.com>
2024-11-08 19:27:53 +0700

    Update CreateCategoryUseCase.java

--- src/main/java/com/simplenotes/domain/usecases/category/CreateCategoryUseCase.java
+++ src/main/java/com/simplenotes/domain/usecases/category/CreateCategoryUseCase.java
@@ -1,5 +1,18 @@
-package main.java.com.simplenotes.domain.usecases.category;
+// CreateCategoryUseCase.java
+package com.simplenotes.domain.usecases.category;
+
+import com.simplenotes.domain.entities.Category;
+import com.simplenotes.domain.repositories.CategoryRepository;
 
 public class CreateCategoryUseCase {
-    
+    private final CategoryRepository categoryRepository;
+
+    public CreateCategoryUseCase(CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
+    }
+
+    public Category execute(String name, String description) {
+        Category category = new Category(null, name, description);
+        return categoryRepository.save(category);
+    }
 }

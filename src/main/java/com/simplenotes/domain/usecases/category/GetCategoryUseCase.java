1f0839136ca79aa459d382c07eddd229d11ad52a
mfaizasysyauqi <109415400+mfaizasysyauqi@users.noreply.github.com>
2024-11-08 19:27:19 +0700

    Update GetCategoryUseCase.java

--- src/main/java/com/simplenotes/domain/usecases/category/GetCategoryUseCase.java
+++ src/main/java/com/simplenotes/domain/usecases/category/GetCategoryUseCase.java
@@ -1,5 +1,17 @@
-package main.java.com.simplenotes.domain.usecases.category;
+// GetCategoryUseCase.java
+package com.simplenotes.domain.usecases.category;
+
+import com.simplenotes.domain.entities.Category;
+import com.simplenotes.domain.repositories.CategoryRepository;
 
 public class GetCategoryUseCase {
-    
+    public final CategoryRepository categoryRepository;
+
+    public GetCategoryUseCase(CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
+    }
+
+    public Category execute(Long id) {
+        return categoryRepository.findById(id);
+    }
 }

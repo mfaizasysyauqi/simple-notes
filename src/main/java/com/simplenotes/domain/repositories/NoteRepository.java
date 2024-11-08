f77d0a20bc631511bc9ea21d9e9d77d0546afb38
mfaizasysyauqi <109415400+mfaizasysyauqi@users.noreply.github.com>
2024-11-08 19:28:23 +0700

    Update NoteRepository.java

--- src/main/java/com/simplenotes/domain/repositories/NoteRepository.java
+++ src/main/java/com/simplenotes/domain/repositories/NoteRepository.java
@@ -1,5 +1,14 @@
-package main.java.com.simplenotes.domain.repositories;
+// NoteRepository.java
+package com.simplenotes.domain.repositories;
 
-public class NoteRepository {
-    
+import java.util.List;
+
+import com.simplenotes.domain.entities.Note;
+
+public interface NoteRepository {
+    Note save(Note note);
+    Note findById(Long id);
+    List<Note> findAll();
+    List<Note> findByCategoryId(Long categoryId);
+    void delete(Long id);
 }

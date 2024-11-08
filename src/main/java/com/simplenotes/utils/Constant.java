54a85a768a0dc4072e90e8041cba2775142b4b0f
mfaizasysyauqi <109415400+mfaizasysyauqi@users.noreply.github.com>
2024-11-08 19:12:35 +0700

    Update Constant.java

--- src/main/java/com/simplenotes/utils/Constant.java
+++ src/main/java/com/simplenotes/utils/Constant.java
@@ -1,5 +1,8 @@
-package main.java.com.simplenotes.utils;
+// src/main/java/com/simplenotes/utils/Constants.java
+package com.simplenotes.utils;
 
-public class Constant {
-    
+public class Constants {
+    public static final String APP_NAME = "SimpleNotes";
+    public static final String DB_NAME = "simplenotes.db";
+    public static final String DB_URL = "jdbc:sqlite:" + DB_NAME;
 }

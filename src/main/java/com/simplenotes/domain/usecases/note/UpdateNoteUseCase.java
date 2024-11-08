b7c138cf26aa4ea9d7683294ac68426bbc3d723a
mfaizasysyauqi <109415400+mfaizasysyauqi@users.noreply.github.com>
2024-11-08 19:25:06 +0700

    Update UpdateNoteUseCase.java

--- src/main/java/com/simplenotes/domain/usecases/note/UpdateNoteUseCase.java
+++ src/main/java/com/simplenotes/domain/usecases/note/UpdateNoteUseCase.java
@@ -1,5 +1,24 @@
-package main.java.com.simplenotes.domain.usecases.note;
+// UpdateNoteUseCase.java
+package com.simplenotes.domain.usecases.note;
+
+import com.simplenotes.domain.entities.Note;
+import com.simplenotes.domain.repositories.NoteRepository;
 
 public class UpdateNoteUseCase {
-    
+    private final NoteRepository noteRepository;
+
+    public UpdateNoteUseCase(NoteRepository noteRepository) {
+        this.noteRepository = noteRepository;
+    }
+
+    public Note execute(Long id, String title, String content, Long categoryId) {
+        Note note = noteRepository.findById(id);
+        if (note != null) {
+            note.setTitle(title);
+            note.setContent(content);
+            note.setCategoryId(categoryId);
+            return noteRepository.save(note);
+        }
+        return null;
+    }
 }

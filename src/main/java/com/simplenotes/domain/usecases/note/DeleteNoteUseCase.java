505d75e91b33ca0ff91780a1d923687ac638abe9
mfaizasysyauqi <109415400+mfaizasysyauqi@users.noreply.github.com>
2024-11-08 19:25:39 +0700

    Update DeleteNoteUseCase.java

--- src/main/java/com/simplenotes/domain/usecases/note/DeleteNoteUseCase.java
+++ src/main/java/com/simplenotes/domain/usecases/note/DeleteNoteUseCase.java
@@ -1,5 +1,16 @@
-package main.java.com.simplenotes.domain.usecases.note;
+// DeleteNoteUseCase.java
+package com.simplenotes.domain.usecases.note;
+
+import com.simplenotes.domain.repositories.NoteRepository;
 
 public class DeleteNoteUseCase {
-    
+    private final NoteRepository noteRepository;
+
+    public DeleteNoteUseCase(NoteRepository noteRepository) {
+        this.noteRepository = noteRepository;
+    }
+
+    public void execute(Long id) {
+        noteRepository.delete(id);
+    }
 }
